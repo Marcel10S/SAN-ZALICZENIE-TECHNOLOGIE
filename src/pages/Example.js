560c3f7d09a4e78cbf89c999c2eb9262fceabdf8
stulczewskim <marcel.stulczewski@evolpe.pl>
2023-11-20 20:53:38 +0100

    Default contnet

--- src/pages/Example.js
+++ src/pages/Example.js
@@ -4,7 +4,20 @@ import Theme from "../theme";
 export default function Example() {
   return (
     <Theme>
-      <div>Example</div>
+      <h1>Witaj na stronie przykładowej</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac lobortis massa. Etiam ullamcorper ante leo, et porta ligula laoreet in. Curabitur vestibulum lorem risus, quis porttitor ante semper euismod. Phasellus metus nisi, tempor sed sapien vel, vestibulum congue nisi. Nulla porta auctor lectus, sed posuere quam accumsan sit amet. Etiam lacinia varius purus eu feugiat. Sed sit amet hendrerit dui. Proin blandit ex elit, quis tincidunt augue posuere non. Cras feugiat bibendum sapien in imperdiet. Nullam elit nunc, hendrerit nec congue non, egestas egestas libero. Cras augue arcu, tempus rhoncus aliquet sed, ultrices id nulla. Curabitur elementum sodales nulla.
+      </p>
+      <p>
+        Donec viverra dolor eget maximus efficitur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc bibendum ac nulla non efficitur. Integer venenatis non lorem quis pretium. Cras aliquet maximus blandit. Sed arcu enim, dapibus sit amet mollis et, interdum sagittis risus. Cras tempus molestie enim, quis ullamcorper sapien gravida et. Sed auctor purus non mi sodales, id pretium lectus malesuada. In condimentum aliquam leo, sit amet rhoncus turpis laoreet quis. In auctor, libero sit amet mollis fringilla, neque eros laoreet metus, fringilla mollis tortor purus quis neque. In quis tortor efficitur, vulputate arcu at, scelerisque purus. Curabitur eu mi odio. Quisque placerat tellus maximus erat cursus, et maximus lorem tincidunt. Nam et congue purus. Fusce malesuada, sem at ullamcorper mollis, sem quam facilisis ante, quis tincidunt metus lacus at arcu. Morbi mollis nisl et magna convallis, scelerisque congue odio iaculis.
+      </p>
+      In hac habitasse platea dictumst. Maecenas enim dui, suscipit nec convallis a, ornare eu nunc. Sed tincidunt mollis felis. Integer vehicula eros maximus, dictum erat non, blandit arcu. Nunc id arcu efficitur, lobortis purus vitae, ultrices augue. Ut at elementum augue. Aenean vestibulum eros eu nisl iaculis, pellentesque tristique ipsum egestas. Morbi dictum vitae augue non vulputate. Integer posuere placerat mauris, quis vestibulum turpis maximus non. Vestibulum eleifend ipsum risus, eu rutrum orci vulputate quis. Vivamus condimentum sollicitudin faucibus.
+      <p>
+        Curabitur placerat et risus vitae ullamcorper. Fusce quis odio consequat, finibus augue non, scelerisque lorem. Quisque sit amet finibus felis. Fusce dignissim in dui eget porta. Morbi eu lorem et urna placerat mattis vel ac purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed auctor eleifend leo, semper tempor lacus facilisis id. Sed vel mollis metus. Vivamus mollis, libero quis iaculis pretium, metus mauris bibendum nibh, ac laoreet odio augue et lectus. Nulla condimentum est vehicula, blandit est sit amet, gravida erat.
+      </p>
+      <p>
+        Sed interdum lacus vitae egestas porta. Fusce feugiat rutrum quam, vel pharetra magna laoreet et. Quisque consectetur neque ut velit lacinia euismod. Cras euismod bibendum nibh, quis fringilla leo. Etiam ac semper nisl. Vivamus mollis malesuada leo nec elementum. Donec nec diam ut turpis cursus tristique. Vestibulum blandit purus in diam tempus venenatis. Donec sodales pulvinar mauris eget fermentum.
+      </p>
     </Theme>
   );
 }
--- src/theme/Footer.js
+++ src/theme/Footer.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 
 export default function Footer() {
+  const footer = useRef(null);
+  const [footerHeight, setFooterHeight] = useState("60px");
+
+  useEffect(() => {
+    if (!footer.current) return;
+    const resizeObserver = new ResizeObserver(() => {
+      setFooterHeight(footer.current.offsetHeight)
+    });
+    resizeObserver.observe(footer.current);
+    return () => resizeObserver.disconnect();
+  }, []);
+
+
   return (
-    <footer className="footer">
-      <div className="footer__content">
-        <div className="footer__content__row">Autor: Marcel Stulczewski</div>
-        <div className="footer__content__row">Index: 118903</div>
-      </div>
-    </footer>
+    <>
+      <footer className="footer" ref={footer}>
+        <div className="footer__content">
+          <div className="footer__content__row">Autor: Marcel Stulczewski</div>
+          <div className="footer__content__row">Index: 118903</div>
+        </div>
+      </footer>
+      <div style={{ height: footerHeight }}></div>
+    </>
   );
 }
